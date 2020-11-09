d55c2be342ddf691122b9660c9e8732f1aca6725
Austin <ausjwill@gmail.com>
2020-11-09 11:16:14 -0500

    model_class

---
+++ RestaurantRater/Models/Restaurant.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantRater.Models
+{
+    public class Restaurant
+    {
+        public int RestaurantID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Rating { get; set; }
+    }
+}
